b68ff3b8932b272e879ed7e47ab0dcc9845827e3
unknown <Kristoffer@.(none)>
2013-05-09 17:09:49 +0200

    Tutsplus example

--- codo-app/app.js
+++ codo-app/app.js
@@ -5,31 +5,22 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , mongo = require('mongodb');
 
-var app = express();
+var app = express(),
+	db = new mongo.Db('codo', new mongo.Server('localhost', 27017)),
+	codoList = db.collection("codolist");
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.configure(function(){
+	app.use(express.bodyParser());	
 });
+
+app.get('/', function(req, res){
+  codoList.find().toArray(function (err, docs) {
+  	if (err) throw err;
+  	res.render("index.jade", { codoList: docs });
+  });
+ });
+
+http.createServer(app).listen(3000);
